c7d9fdab713080ff8b505eec884892755bb45e3c
ezenard <ezenardr.dev@gmail.com>
2026-01-05 13:16:47 -0500

    Feat: mobile nav

--- apps/website/src/components/Navbar.tsx
+++ apps/website/src/components/Navbar.tsx
@@ -3,8 +3,22 @@ import Logo from "@/assets/images/logo-wide-yellow.svg";
 import { Link, usePathname } from "@/i18n/navigation";
 import Image from "next/image";
 import { motion } from "motion/react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { CloseCircle, HamburgerMenu } from "iconsax-reactjs";
+import { useTranslations } from "next-intl";
+import SwitchLocale from "./SwitchLocale";
 
 export default function Navbar() {
+  const t = useTranslations("HomePage.navbar");
   const pathname = usePathname();
   function isActive(href: string) {
     return href === pathname;
@@ -28,7 +42,7 @@ export default function Navbar() {
             {isActive("/attendee") && (
               <div className="w-3 h-3 rounded-full bg-primary-500"></div>
             )}
-            <Link href="/attendee">Personal</Link>
+            <Link href="/attendee">{t("personal")}</Link>
           </li>
           <li
             className={`px-[1.5rem] py-[5px] flex items-center gap-3 bg-neutral-100 rounded-[100px] text-[1.6rem]  leading-[22.5px] ${isActive("/business") ? "text-primary-500 font-medium" : "text-deep-100 font-normal"} hover:text-primary-500`}
@@ -36,7 +50,7 @@ export default function Navbar() {
             {isActive("/business") && (
               <div className="w-3 h-3 rounded-full bg-primary-500"></div>
             )}
-            <Link href="/business">Business</Link>
+            <Link href="/business">{t("business")}</Link>
           </li>
           <li
             className={`px-[1.5rem] py-[5px] flex items-center gap-3 bg-neutral-100 rounded-[100px] text-[1.6rem]  leading-[22.5px] ${isActive("/about") ? "text-primary-500 font-medium" : "text-deep-100 font-normal"} hover:text-primary-500`}
@@ -44,7 +58,7 @@ export default function Navbar() {
             {isActive("/about") && (
               <div className="w-3 h-3 rounded-full bg-primary-500"></div>
             )}
-            <Link href="/about">About us</Link>
+            <Link href="/about">{t("about")}</Link>
           </li>
           <li
             className={`px-[1.5rem] py-[5px] flex items-center gap-3 bg-neutral-100 rounded-[100px] text-[1.6rem]  leading-[22.5px] ${isActive("/contact") ? "text-primary-500 font-medium" : "text-deep-100 font-normal"} hover:text-primary-500`}
@@ -52,11 +66,96 @@ export default function Navbar() {
             {isActive("/contact") && (
               <div className="w-3 h-3 rounded-full bg-primary-500"></div>
             )}
-            <Link href="/contact">Contact us</Link>
+            <Link href="/contact">{t("contact")}</Link>
           </li>
         </ul>
       </nav>
-      <div>lang</div>
+      <Sheet>
+        <SheetTrigger asChild className={"cursor-pointer lg:hidden"}>
+          <div className={"flex items-center gap-4 cursor-pointer"}>
+            <HamburgerMenu size="20" color="#737C8A" variant="Bulk" />
+            <span className={"text-[1.5rem] leading-[20px] text-neutral-700"}>
+              Menu
+            </span>
+          </div>
+        </SheetTrigger>
+        <SheetContent
+          side={"top"}
+          className={
+            "flex flex-col gap-[50px] p-[15px] mt-2 mx-2 pb-0 rounded-[25px]"
+          }
+        >
+          <SheetHeader>
+            <SheetTitle className={"sr-only"}>test</SheetTitle>
+            <SheetDescription className={"sr-only"}>test</SheetDescription>
+            <div className={"flex w-full items-center justify-between"}>
+              <Link href={"/"}>
+                <Image
+                  width={140}
+                  height={40}
+                  src={Logo}
+                  alt={"Logo of TicketWaze"}
+                  className={"w-[140px] h-[40px]"}
+                  priority
+                  loading={"eager"}
+                />
+              </Link>
+              <SheetClose>
+                <div className={"flex items-center gap-4 cursor-pointer"}>
+                  <CloseCircle size="20" color="#737C8A" variant="Bulk" />
+                  <span
+                    className={"text-[1.5rem] leading-[20px] text-neutral-700"}
+                  >
+                    {t("close")}
+                  </span>
+                </div>
+              </SheetClose>
+            </div>
+          </SheetHeader>
+          <ul className=" grid grid-cols-2 gap-8">
+            <li
+              className={`px-[7.5px] py-[2.5px] flex items-center text-center justify-center gap-3 rounded-[100px] text-[1.8rem]  leading-[25px] ${isActive("/attendee") ? "text-primary-500 font-medium bg-neutral-200/80" : "text-deep-100 font-normal"} hover:text-primary-500`}
+            >
+              {isActive("/attendee") && (
+                <div className="w-3 h-3 rounded-full bg-primary-500"></div>
+              )}
+              <Link href="/attendee">{t("personal")}</Link>
+            </li>
+            <li
+              className={`px-[7.5px] py-[2.5px] flex items-center text-center justify-center gap-3 rounded-[100px] text-[1.8rem]  leading-[25px] ${isActive("/business") ? "text-primary-500 font-medium bg-neutral-200/80" : "text-deep-100 font-normal"} hover:text-primary-500`}
+            >
+              {isActive("/business") && (
+                <div className="w-3 h-3 rounded-full bg-primary-500"></div>
+              )}
+              <Link href="/business">{t("business")}</Link>
+            </li>
+            <li
+              className={`px-[7.5px] py-[2.5px] flex items-center text-center justify-center gap-3 rounded-[100px] text-[1.8rem]  leading-[25px] ${isActive("/about") ? "text-primary-500 font-medium bg-neutral-200/80" : "text-deep-100 font-normal"} hover:text-primary-500`}
+            >
+              {isActive("/about") && (
+                <div className="w-3 h-3 rounded-full bg-primary-500"></div>
+              )}
+              <Link href="/about">{t("about")}</Link>
+            </li>
+            <li
+              className={`px-[7.5px] py-[2.5px] flex items-center text-center justify-center gap-3 rounded-[100px] text-[1.8rem]  leading-[25px] ${isActive("/contact") ? "text-primary-500 font-medium bg-neutral-200/80" : "text-deep-100 font-normal"} hover:text-primary-500`}
+            >
+              {isActive("/contact") && (
+                <div className="w-3 h-3 rounded-full bg-primary-500"></div>
+              )}
+              <Link href="/contact">{t("contact")}</Link>
+            </li>
+          </ul>
+          <SheetFooter>
+            <div>
+              <SwitchLocale />
+            </div>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+      <div className="hidden lg:block">
+        <SwitchLocale />
+      </div>
     </motion.header>
   );
 }
---
+++ apps/website/src/components/SwitchLocale.tsx
@@ -0,0 +1,60 @@
+import { usePathname, useRouter } from "next/navigation";
+import { useLocale } from "next-intl";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/LanguageSelect";
+import Image from "next/image";
+import US from "@/assets/flags/us.svg";
+import FR from "@/assets/flags/fr.svg";
+import { ArrowDown2 } from "iconsax-reactjs";
+import { useTranslations } from "next-intl";
+
+export default function SwitchLocale() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+
+  const switchLocale = (newLocale: string) => {
+    const segments = pathname.split("/");
+    segments[1] = newLocale;
+    router.push(segments.join("/"));
+  };
+  return (
+    <Select onValueChange={(e) => switchLocale(e)}>
+      <SelectTrigger>
+        {locale === "en" ? (
+          <Image src={US} alt={"us flag"} width={30} height={30} />
+        ) : (
+          <Image src={FR} alt={"french flag"} width={30} height={30} />
+        )}
+        <span
+          className={
+            "text-[1.4rem] leading-[20px] font-medium uppercase text-deep-100"
+          }
+        >
+          {locale}
+        </span>
+        <ArrowDown2 size="20" color="#000000" variant="Bulk" />
+      </SelectTrigger>
+      <SelectContent className={"bg-neutral-100"}>
+        {/* <SelectItem
+          className={`${locale === "fr" ? "bg-neutral-300 hover:bg-neutral-300" : ""} flex items-center gap-4`}
+          value="fr"
+        >
+          <Image src={FR} alt={"french flag"} width={30} height={30} />
+          <span>Français</span>
+        </SelectItem> */}
+        <SelectItem
+          className={`${locale === "en" ? "bg-neutral-300 hover:bg-neutral-300" : ""} flex items-center gap-4`}
+          value="en"
+        >
+          <Image src={US} alt={"us flag"} width={30} height={30} />
+          <span>English</span>
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
