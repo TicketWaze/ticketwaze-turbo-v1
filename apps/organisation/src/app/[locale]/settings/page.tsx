25d90d7938226f907daf182030db978aa529f946
ezenard <ezenardr.dev@gmail.com>
2026-01-25 07:21:18 -0500

    Feat: organisation dashboard init

--- apps/organisation/src/app/[locale]/settings/page.tsx
+++ apps/organisation/src/app/[locale]/settings/page.tsx
@@ -2,7 +2,6 @@ import OrganizerLayout from "@/components/Layouts/OrganizerLayout";
 import { SimpleTopbar } from "@/components/Layouts/Topbars";
 import { Link } from "@/i18n/navigation";
 import {
-  Add,
   ArrowRight2,
   CardPos,
   I24Support,
@@ -63,11 +62,40 @@ export default async function Settings() {
           );
         })}
         <li className="lg:hidden">
-          <SettingsCardLink
-            Icon={I24Support}
+          <Link
+            target={"_blank"}
+            rel={"noreferrer"}
             href={`${process.env.NEXT_PUBLIC_WEBSITE_URL}/${locale}/contact`}
-            label={t("help")}
-          />
+            className={
+              "py-14 px-6 rounded-[10px] bg-neutral-100 hover:bg-primary-50 flex justify-between transition-all duration-500 cursor-pointer group"
+            }
+          >
+            <div className={"flex items-center gap-6"}>
+              <I24Support
+                size="25"
+                className=" transition-all duration-500 stroke-neutral-900 fill-neutral-900 group-hover:stroke-primary-500 group-hover:fill-primary-500"
+                variant="Bulk"
+              />
+              <span
+                className={
+                  "font-primary font-medium text-[2.2rem] transition-all duration-500 leading-12 text-neutral-900 group-hover:text-primary-500"
+                }
+              >
+                {t("help")}
+              </span>
+            </div>
+            <div
+              className={
+                "w-14 h-14 rounded-full flex items-center justify-center transition-all duration-500 bg-neutral-200 group-hover:bg-primary-100"
+              }
+            >
+              <ArrowRight2
+                size="20"
+                className=" transition-all duration-500 stroke-neutral-900 fill-neutral-900 group-hover:stroke-primary-500 group-hover:fill-primary-500"
+                variant="Bulk"
+              />
+            </div>
+          </Link>
         </li>
         <SwitchOrganisationMobile />
         {/* <li className="lg:hidden">
