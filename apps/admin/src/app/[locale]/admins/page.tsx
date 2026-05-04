72f0e47f63c2c61d0d57c1241d8aa18fff1f1369
ezenardr <ezenardr.dev@gmail.com>
2026-05-04 14:04:40 -0400

    Feat: administrator

--- apps/admin/src/app/[locale]/admins/page.tsx
+++ apps/admin/src/app/[locale]/admins/page.tsx
@@ -1,5 +1,34 @@
 import AdminLayout from "@/components/Layouts/AdminLayout";
+import { auth } from "@/lib/auth";
+import MemberList from "./components/MemberList";
+import BackButton from "@/components/shared/BackButton";
+import TopBar from "@/components/shared/TopBar";
+import { getTranslations } from "next-intl/server";
 
-export default function AdminsPage() {
-  return <AdminLayout>AdminsPage</AdminLayout>;
+export default async function AdminsPage() {
+  const session = await auth();
+  const t = await getTranslations("Admins");
+  const request = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/admin/administrator`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session?.user.accessToken}`,
+      },
+    },
+  );
+  const response = await request.json();
+  console.log(response);
+  return (
+    <AdminLayout>
+      <div className="flex flex-col gap-8">
+        <BackButton text={t("back")} />
+        <TopBar title={t("title")}>
+          {/* {authorized && <AddMember totalMembers={totalMembers} />} */}
+        </TopBar>
+      </div>
+      <MemberList users={response.users} />
+    </AdminLayout>
+  );
 }
--- packages/typescript-config/types.d.ts
+++ packages/typescript-config/types.d.ts
@@ -263,6 +263,7 @@ export interface User {
   organisations: Organisation[];
   userPreference: UserPreference;
   isOnboarded: boolean;
+  role: number;
 }
 
 export interface Currency {
