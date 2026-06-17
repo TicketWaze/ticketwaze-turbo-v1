ffb80a4bb8d58dacf5ead05d9aa1978e8e717d78
ezenardr <ezenardr.dev@gmail.com>
2026-06-17 10:32:33 -0400

    Feat: admin analytics page

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -3,9 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental: {
-    allowedDevOrigins: ["*"],
-  },
   images: {
     remotePatterns: [
       {
